d0418822eb252fa5a0f008795742ff842a73d289
wsi1212 <wsi1212@dgsw.hs.kr>
2023-08-21 14:08:37 +0900

    Add: Splash screen
    8/21

---
+++ presentation/src/main/java/kr/hs/dgsw/mentomenv2/IntroActivity.kt
@@ -0,0 +1,24 @@
+package kr.hs.dgsw.mentomenv2
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+class IntroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intro)
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        finish()
+    }
+}
