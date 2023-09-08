2705790c52fc106601548514c1ae7d74238bdaa3
wsi1212 <wsi1212@dgsw.hs.kr>
2023-09-08 15:07:33 +0900

    Fix: Version error
    9/8

--- presentation/src/main/java/kr/hs/dgsw/mentomenv2/feature/auth/login/LoginActivity.kt
+++ presentation/src/main/java/kr/hs/dgsw/mentomenv2/feature/auth/login/LoginActivity.kt
@@ -1,10 +1,14 @@
 package kr.hs.dgsw.mentomenv2.feature.auth.login
 
+import dagger.hilt.android.AndroidEntryPoint
 import kr.hs.dgsw.mentomenv2.R
 import kr.hs.dgsw.mentomenv2.base.BaseActivity
 import kr.hs.dgsw.mentomenv2.databinding.ActivityLoginBinding
 
-class LoginActivity(override val viewModel: LoginViewModel) : BaseActivity<ActivityLoginBinding, LoginViewModel>(R.layout.activity_login) {
+@AndroidEntryPoint
+class LoginActivity() : BaseActivity<ActivityLoginBinding, LoginViewModel>(R.layout.activity_login) {
+    override val viewModel: LoginViewModel by viewModels()
+
     override fun start() {
 
     }
--- presentation/src/main/java/kr/hs/dgsw/mentomenv2/feature/auth/login/LoginViewModel.kt
+++ presentation/src/main/java/kr/hs/dgsw/mentomenv2/feature/auth/login/LoginViewModel.kt
@@ -1,7 +1,16 @@
 package kr.hs.dgsw.mentomenv2.feature.auth.login
 
+import androidx.lifecycle.MutableLiveData
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kr.hs.dgsw.mentomenv2.base.BaseViewModel
 
+@HiltViewModel
 class LoginViewModel : BaseViewModel() {
+    val profileImage = MutableLiveData<String?>()
+    val id = MutableLiveData<String>()
+    val pw = MutableLiveData<String>()
 
+    fun onClickLogin() {
+
+    }
 }
