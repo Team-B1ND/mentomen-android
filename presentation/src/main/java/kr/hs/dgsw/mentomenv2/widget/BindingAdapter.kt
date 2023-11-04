f2e566491efae253349f6a4eff06e02f421f9476
wsi1212 <wsi1212@dgsw.hs.kr>
2023-11-04 15:17:30 +0900

    Add: BindingAdapter
    11/4

--- presentation/src/main/java/kr/hs/dgsw/mentomenv2/widget/BindingAdapter.kt
+++ presentation/src/main/java/kr/hs/dgsw/mentomenv2/widget/BindingAdapter.kt
@@ -1,16 +1,50 @@
 package kr.hs.dgsw.mentomenv2.widget
 
-import android.content.ContentValues.TAG
-import android.util.Log
 import android.widget.Button
 import androidx.databinding.BindingAdapter
-import androidx.lifecycle.MutableLiveData
-import kr.hs.dgsw.mentomenv2.util.getParentActivity
+import kr.hs.dgsw.mentomenv2.R
 
 @BindingAdapter("designButtonState")
-fun setDesignButtonState(view: Button, tagState: MutableLiveData<String>) {
-    val parentActivity = view.getParentActivity() ?: return
-    tagState.observe(parentActivity) {
-        Log.d(TAG, "setDesignButtonState: $it")
+fun setDesignButtonState(view: Button, tagState: String) {
+    if (tagState == "DESIGN" || tagState == "ALL") {
+        view.setBackgroundResource(R.drawable.bg_selected_design)
+    } else {
+        view.setBackgroundResource(R.drawable.bg_unselected_tag)
+    }
+}
+
+@BindingAdapter("webButtonState")
+fun setWebButtonState(view: Button, tagState: String) {
+    if (tagState == "WEB" || tagState == "ALL") {
+        view.setBackgroundResource(R.drawable.bg_selected_web)
+    } else {
+        view.setBackgroundResource(R.drawable.bg_unselected_tag)
+    }
+}
+
+@BindingAdapter("androidButtonState")
+fun setAndroidButtonState(view: Button, tagState: String) {
+    if (tagState == "ANDROID" || tagState == "ALL") {
+        view.setBackgroundResource(R.drawable.bg_selected_android)
+    } else {
+        view.setBackgroundResource(R.drawable.bg_unselected_tag)
+    }
+}
+
+@BindingAdapter("serverButtonState")
+fun setServerButtonState(view: Button, tagState: String) {
+    if (tagState == "SERVER" || tagState == "ALL") {
+        view.setBackgroundResource(R.drawable.bg_selected_server)
+    } else {
+        view.setBackgroundResource(R.drawable.bg_unselected_tag)
+    }
+}
+
+@BindingAdapter("iosButtonState")
+fun setIosButtonState(view: Button, tagState: String) {
+    if (tagState == "IOS" || tagState == "ALL") {
+        view.setBackgroundResource(R.drawable.bg_selected_ios)
+    } else {
+        view.setBackgroundResource(R.drawable.bg_unselected_tag)
     }
 }
