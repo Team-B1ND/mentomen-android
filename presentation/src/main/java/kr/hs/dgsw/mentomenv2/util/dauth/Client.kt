dff01953b6c073ab974a1496236f162b9542c756
wsi1212 <wsi1212@dgsw.hs.kr>
2023-11-14 16:29:27 +0900

    Fix: gitignore
    11/14

--- presentation/src/main/java/kr/hs/dgsw/mentomenv2/util/dauth/Client.kt
+++
@@ -1,7 +0,0 @@
-package kr.hs.dgsw.mentomenv2.util.dauth
-
-object Client {
-    const val clientId = "39bc523458c14eb987b7b16175426a31a9f105b7f5814f1f9eca7d454bd23c73"
-    const val clientSecret = "e90b070b437f420eb788fad746e97a507984328ddf9142f481397ca6e7afda0e"
-    const val redirectUri = "http://localhost:3000/callback"
-}
