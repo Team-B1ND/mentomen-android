d359e076e4ce6cc89e3197bf2bd1bb25ff90db93
wsi1212 <wsi1212@dgsw.hs.kr>
2023-11-14 23:27:57 +0900

    Delete: Duplicated Implementation
    11/14

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -79,8 +79,6 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.5.1")
     implementation("com.google.android.material:material:1.10.0")
 
-    implementation("androidx.navigation:navigation-fragment-ktx:2.3.5")
-    implementation("androidx.navigation:navigation-ui-ktx:2.3.5")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
