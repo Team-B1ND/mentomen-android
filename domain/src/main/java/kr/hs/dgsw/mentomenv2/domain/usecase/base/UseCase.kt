ff9a7fadda13fefb7d77742e381c4724727312c2
wsi1212 <wsi1212@dgsw.hs.kr>
2023-12-04 10:57:24 +0900

    Delete: BaseUseCase
    12/4

--- domain/src/main/java/kr/hs/dgsw/mentomenv2/domain/usecase/base/UseCase.kt
+++
@@ -1,8 +0,0 @@
-package kr.hs.dgsw.mentomenv2.domain.usecase.base
-
-import kotlinx.coroutines.flow.Flow
-import kr.hs.dgsw.mentomenv2.domain.util.Result
-
-abstract class UseCase<PR, R> {
-    abstract operator fun invoke(params: PR): Flow<Result<R>>
-}
