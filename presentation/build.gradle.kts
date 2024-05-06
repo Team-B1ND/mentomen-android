ef97e2bcfeb5577bdb868d7b653678933052f81c
wsi1212 <wsi1212@dgsw.hs.kr>
2024-05-07 08:42:01 +0900

    Fix: Reset version code
    5/7

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -29,7 +29,7 @@ android {
         applicationId = "com.b1nd.mentomen"
         minSdk = 26
         targetSdk = 34
-        versionCode = 10002
+        versionCode = 10000
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
